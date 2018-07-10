52ce6a19a2b31dec15111caf16fd99e5e04a5be4
Yang Li <yang@truman.edu>
2018-07-10 13:58:20 -0400

    clean up

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,52 +2,62 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  items: [
+  wheel: [
     {
-      src: '/assets/images/red/web-ready_OA-2017_Readers_Engaging the Sustained Argument.jpg',
-      w: 1296,
-      h: 2160,
+      src: '/assets/images/home/print-quality_OA-2017_Immediate and Free.jpg',
+      w: 1350,
+      h: 2250,
     },
     {
-      src: '/assets/images/red/web-ready_OA-2017_Readers_Reducing Barriers to Access.jpg',
-      w: 1296,
-      h: 2160,
+      src: '/assets/images/home/print-quality_OA-2017_Readers_Reducing Barriers to Access.jpg',
+      w: 1350,
+      h: 2250,
     },
     {
-      src: '/assets/images/red/web-ready_OA-2017_Readers_Sharing and Using.jpg',
-      w: 1296,
-      h: 2160,
+      src: '/assets/images/home/print-quality_OA-2017_Readers_Sharing and Using.jpg',
+      w: 1350,
+      h: 2250,
+    },
+    {
+      src: '/assets/images/home/print-quality_OA-2017_Readers_Engaging the Sustained Argument.jpg',
+      w: 1350,
+      h: 2250,
+    },
+    {
+      src: '/assets/images/home/print-quality_OA-2017_New Possibilities of Digital Environments.jpg',
+      w: 1350,
+      h: 2250,
+    },
+    {
+      src: '/assets/images/home/print-quality_OA-2017_Authors_Engaging the Humanities.jpg',
+      w: 1350,
+      h: 2250,
+    },
+    {
+      src: '/assets/images/home/print-quality_OA-2017_Authors_Reducing Market Barriers.jpg',
+      w: 1350,
+      h: 2250,
+    },
+    {
+      src: '/assets/images/home/print-quality_OA-2017_Authors_Widening Potential Readership.jpg',
+      w: 1350,
+      h: 2250,
     },
   ],
 
-  black: [
+  author: [
     {
-      src: '/assets/images/black/web-ready_OA-2017_Immediate and Free.jpg',
-      w: 1296,
-      h: 2160,
-    },
-    {
-      src: '/assets/images/black/web-ready_OA-2017_New Possibilities of Digital Environments.jpg',
-      w: 1296,
-      h: 2160,
+      src: '/assets/images/cover/web-ready_OA-2017_Authors_Credibility Economic Free Rider.jpg',
+      w: 2592,
+      h: 1710,
     },
   ],
 
-  orange: [
+  reader: [
     {
-      src: '/assets/images/orange/web-ready_OA-2017_Authors_Engaging the Humanities.jpg',
-      w: 1296,
-      h: 2160,
-    },
-    {
-      src: '/assets/images/orange/web-ready_OA-2017_Authors_Reducing Market Barriers.jpg',
-      w: 1296,
-      h: 2160,
-    },
-    {
-      src: '/assets/images/orange/web-ready_OA-2017_Authors_Widening Potential Readership.jpg',
-      w: 1296,
-      h: 2160,
+      src: '/assets/images/cover/web-ready_OA-2017_Readers_1-of-2_Authenticity Version Print Long-term Access.jpg',
+      w: 2592,
+      h: 2304,
     },
   ],
 
